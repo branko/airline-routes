4927b7038abed58af189c2289d1d4171f9ddf843
Jim Benton <jim@autonomousmachine.com>
2017-11-28 11:57:48 -0600

    Add project template.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="header">
+          <h1 className="title">Airline Routes</h1>
+        </header>
+        <section>
+          <p>
+            Welcome to the app!
+          </p>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
